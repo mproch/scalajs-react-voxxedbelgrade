8b083448eeeff8512cc767f67068a94de555662d
Maciek Próchniak <mpr@touk.pl>
2015-09-27 19:09:41 +0200

    basic

--- src/main/scala/pl/touk/scalajs/HelloWorld.scala
+++ src/main/scala/pl/touk/scalajs/HelloWorld.scala
@@ -1,5 +1,31 @@
 package pl.touk.scalajs
 
+import scala.scalajs.js.annotation.JSExport
+import scalatags.JsDom.all._
+
+import org.scalajs.dom.document
+
+@JSExport
 class HelloWorld {
+
+  @JSExport
+  def run(): Unit = {
+    println("hello world")
+
+    val inputEl = input(`type` := "text", id := "input").render
+
+    def append(): Unit = {
+      document.getElementById("value").textContent = inputEl.value
+    }
+
+    val content = div(
+      inputEl,
+      button("Submit", onclick := append _),
+      div("The value you entered is: "),
+      b(span(id:="value"))
+    )
+
+    document.getElementById("content").appendChild(content.render)
+  }
 
 }
