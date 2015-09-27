41196da0da77e1b555981abb9ab932e661b01c33
Maciek Próchniak <mpr@touk.pl>
2015-09-27 23:06:25 +0200

    react2

--- js/src/main/scala/pl/touk/scalajs/HelloWorld.scala
+++ js/src/main/scala/pl/touk/scalajs/HelloWorld.scala
@@ -1,19 +1,17 @@
 package pl.touk.scalajs
 
 import japgolly.scalajs.react.vdom.prefix_<^._
-import japgolly.scalajs.react.{React, ReactComponentB}
+import japgolly.scalajs.react.{BackendScope, React, ReactComponentB, _}
 import org.scalajs.dom
 import org.scalajs.dom.document
 import org.scalajs.dom.ext.Ajax
 import pl.mproch.openlayers.OpenLayers.lonLatToMercator
-import pl.mproch.openlayers._
 import upickle.default._
 
 import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 import scala.scalajs.js.annotation.JSExport
 import scala.util.Failure
-import scalatags.JsDom.all._
 import scalaz.OptionT._
 import scalaz.std.scalaFuture.futureInstance
 
@@ -27,14 +25,36 @@ class HelloWorld {
       getData[List[Comment]](s"http://localhost:8080/locationComment/${location.name}").map(location.addComments))
     .run
 
+  class Backend($:BackendScope[Unit, (String, Option[GeoLocation])]) {
+    def valueChanged(event: ReactEventI): Unit = {
+      $.modState(_.copy(_1 = event.target.value))
+    }
+
+    def search(): Unit = {
+      getLocation($.state._1).foreach(locationO => $.modState(_.copy(_2 = locationO)))
+    }
+
+  }
+
   val GeoLocationRender = ReactComponentB[Option[GeoLocation]]("GeoLocationWithComments")
     .render(locationO => <.div(
       locationO.isEmpty ?= <.span("Sorry, no results found"),
         locationO.map(location => <.div(<.b(location.name), <.ul(
           location.comments.map(comm => <.li(comm.toString))
-        )))
+          ),
+          LayerMarker.component(location.lonLat)
+        ))
       ))
     .build
+
+  val LocationSearcher = ReactComponentB[Unit]("LocationSearcher")
+    .initialState(("", Option.empty[GeoLocation]))
+    .backend(new Backend(_))
+    .render((_, S, B) => <.div(
+      <.input(^.`type`:="text", ^.value:=S._1, ^.onChange ==> B.valueChanged),
+      <.button("Search", ^.onClick --> B.search),
+      GeoLocationRender(S._2)
+  )).buildU
 
 
   private def getData[T: Reader](url: String): Future[T]
@@ -48,29 +68,7 @@ class HelloWorld {
   @JSExport
   def run(): Unit = {
     println("hello world")
-
-    val inputEl = input(`type` := "text", id := "input").render
-
-    val map = OSMMap(target = null, layers = List(TileLayer(source = new OSM)), view = View(zoom = 10,
-      center = (0.0, 0.0)))
-
-    def append(): Unit = {
-      val search = inputEl.value
-      getLocation(search).foreach(location => {
-        React.render(GeoLocationRender(location), document.getElementById("holder"))
-        location.foreach(loc => map.getView().setCenter(loc.lonLat))
-      })
-    }
-
-    val content = div(
-      inputEl,
-      button("Submit", onclick := append _),
-      div(id:="holder"),
-      div(height:="200px", width:="200px", id:="map")
-    )
-
-    document.getElementById("content").appendChild(content.render)
-    map.setTarget("map")
+    React.render(LocationSearcher(), document.getElementById("content"))
   }
 
 }
